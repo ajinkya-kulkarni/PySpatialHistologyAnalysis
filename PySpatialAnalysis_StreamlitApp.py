fa70f9c8379be6519434f45182aa08021a2cb658
Ajinkya Kulkarni <kulkajinkya@gmail.com>
2023-03-14 15:37:12 +0100

    Testing out the `wide` mode

--- PySpatialAnalysis_StreamlitApp.py
+++ PySpatialAnalysis_StreamlitApp.py
@@ -44,8 +44,8 @@ with open("logo.jpg", "rb") as f:
 
 image_bytes = BytesIO(image_data)
 
-which_layout = "centered"
-# which_layout = "wide"
+# which_layout = "centered"
+which_layout = "wide"
 
 st.set_page_config(page_title = 'PySpatialAnalysis', page_icon = image_bytes, layout = which_layout, initial_sidebar_state = "expanded", menu_items = {'Get help': 'mailto:ajinkya.kulkarni@mpinat.mpg.de', 'Report a bug': 'mailto:ajinkya.kulkarni@mpinat.mpg.de', 'About': 'This is a application for demonstrating the PySpatialAnalysis package. Developed, tested and maintained by Ajinkya Kulkarni: https://github.com/ajinkya-kulkarni at the MPI-NAT, Goettingen'})
 
@@ -61,7 +61,7 @@ st.markdown("")
 
 with st.form(key = 'form1', clear_on_submit = True):
 
-	st.markdown(':blue[Upload an H&E image to be analyzed. Works best for images smaller than 1000x1000 pixels ]')
+	st.markdown(':blue[Upload an H&E image/slide to be analyzed. Works best for images/slides smaller than 1000x1000 pixels ]')
 
 	uploaded_file = st.file_uploader("Upload a file", type=["tif", "tiff", "png", "jpg", "jpeg"], accept_multiple_files = False, label_visibility = 'collapsed')
 
@@ -90,45 +90,33 @@ with st.form(key = 'form1', clear_on_submit = True):
 
 			# Convert grayscale image to RGB image
 			modified_labels_rgb_image = 255 * np.ones((*modified_labels.shape, 3), dtype=np.uint8)
-			# modified_labels_rgb_image[:, :, 0] = 255 - modified_labels
-			# modified_labels_rgb_image[:, :, 1] = 255 - modified_labels
-			# modified_labels_rgb_image[:, :, 2] = 255 - modified_labels
 
 			# Replace black pixels with "tab:blue"
 			black_pixels = np.where(modified_labels == 255)
 			modified_labels_rgb_image[black_pixels[0], black_pixels[1], :] = (31, 119, 180)
 
-			# Replace white pixels with "cosmic latte"
+			# Replace white pixels with custom RGB color
 			white_pixels = np.where(modified_labels == 0)
 			modified_labels_rgb_image[white_pixels[0], white_pixels[1], :] = (247, 234, 199)
 			
-			image_comparison(
-			img1 = rgb_image,
-			img2 = modified_labels_rgb_image,
-			label1="Image",
-			label2="Result",
-			width = 674,
-			in_memory = True, show_labels = True, make_responsive = True
-			)
-			
-			# if which_layout == "centered:
-			# 	image_comparison(
-			# 	img1 = rgb_image,
-			# 	img2 = modified_labels_rgb_image,
-			# 	label1="Image",
-			# 	label2="Result",
-			# 	width = 674,
-			# 	in_memory = True, show_labels = True, make_responsive = True
-			# 	)
-			# if which_layout == "wide:
-			# 	image_comparison(
-			# 	img1 = rgb_image,
-			# 	img2 = modified_labels_rgb_image,
-			# 	label1="Image",
-			# 	label2="Result",
-			# 	width = 1169,
-			# 	in_memory = True, show_labels = True, make_responsive = True
-			# 	)
+			if which_layout == "centered:
+				image_comparison(
+				img1 = rgb_image,
+				img2 = modified_labels_rgb_image,
+				label1="Image",
+				label2="Result",
+				width = 674,
+				in_memory = True, show_labels = True, make_responsive = True
+				)
+			if which_layout == "wide:
+				image_comparison(
+				img1 = rgb_image,
+				img2 = modified_labels_rgb_image,
+				label1="Image",
+				label2="Result",
+				width = 1169,
+				in_memory = True, show_labels = True, make_responsive = True
+				)
 
 		except:
 
